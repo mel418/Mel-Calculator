7c0faf0ea23a8d645f61939f4687da3f7f76bbcc
mel148 <melodygatan@gmail.com>
2023-07-11 23:00:36 -0700

    operate function and display

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const number1 = 34;
-const operator = '+';
-const number2 = 35;
+let number1 = 34;
+let operator = '+';
+let number2 = 35;
+let result = operate(number1, number2, operator)
+let solution = number1 + ' ' + operator + ' ' + number2 + ' = ' + result;
 
+function updateDisplay() {
+    const display = document.getElementById('display');
+    display.innerText = solution;
+}
+
+updateDisplay();
 
 function add(num1, num2) {
     return num1 + num2;
@@ -22,6 +30,20 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate() {
-    
+function operate(x, y, op) {
+    if (op === '+') {
+        return add(x,y)
+    }
+    else if (op === '-') {
+        return subtract(x,y)
+    }
+    else if (op === '*') {
+        return multiply(x,y)
+    }
+    else {
+        return divide(x,y)
+    }
 }
+
+
+console.log(result)
