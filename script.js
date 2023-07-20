e171a13e9afe7eaded208469f3094c76c5f2242f
mel148 <melodygatan@gmail.com>
2023-07-20 00:46:25 -0700

    completed calculator^^

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
+//MODEL
 let number1 = null;
 let operator1 = null;
 let operator2 = null;
 let number2 = null;
 let result = null
 let displayValue = '0';
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+  
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if (num2 === 0) {
+        return "Error: Mommy Zero!";
+    }
+    return num1 / num2;
+}
+
+function operate(x, y, op) {
+    if (op === '+') {
+        return add(x,y)
+    }
+    else if (op === '-') {
+        return subtract(x,y)
+    }
+    else if (op === '*') {
+        return multiply(x,y)
+    }
+    else {
+        return divide(x,y)
+    }
+}
+
+function roundLongNumber(num, places) {
+    return parseFloat(Math.round(num + 'e' + places) + 'e-' + places);
+}
+
+
+//CONTROLLER
 const buttons = document.querySelectorAll("button");
 
 window.addEventListener('keydown', function(e) {
     const key = document.querySelector((`button[data-key="${e.keyCode}"]`));
     key.click();
     displayValue = key.value;
-    console.log(displayValue)
 });
-
-
 
 
 function updateDisplay() {
@@ -22,13 +61,14 @@ function updateDisplay() {
     if (displayValue.length > 9) {
         display.innerText = displayValue.substring(0, 9);
     }
+    console.log(display)
 }
 
 updateDisplay();
 
 // button click event handling
 function clickButton() {  
-    for(let i = 0;  i<buttons.length; i++) {
+    for(let i = 0;  i < buttons.length; i++) {
         buttons[i].addEventListener('click', function() {
             if (buttons[i].classList.contains('operand')) {
                 inputOperand(buttons[i].value);
@@ -87,7 +127,7 @@ function inputOperator(operator) {
     if (operator1 != null && operator2 === null) {
         operator2 = operator;
         number2 = displayValue;
-        result = operate(Nummber(number1), Number(number2), operator1);
+        result = operate(Number(number1), Number(number2), operator1);
         displayValue = roundLongNumber(result, 15).toString();
         number1 = displayValue;
         result= null;
@@ -116,68 +156,55 @@ function inputEquals() {
         if(result === 'Error: Mommy Zero!') {
             displayValue = 'Error: Mommy Zero!';
         }
+        else {
+            displayValue = roundLongNumber(result, 15).toString();
+            number1 = displayValue;
+            number2 = null;
+            operator1 = null;
+            operator2 = null;
+            result=null;
+        }
     }
     else {
-        displayValue = roundLongNumber(result, 15).toString();
-        number1 = displayValue;
-        number2 = null;
-        operator1 = null;
-        operator2 = null;
-        result = null;
+        number2 = displayValue;
+        result = operate(Number(number1), Number(number2), operator1)
+        if(result === 'Error: Mommy Zero!') {
+            displayValue = 'Error: Mommy Zero!';
+        }
+        else {
+            displayValue = roundLongNumber(result, 15).toString();
+            number1 = displayValue;
+            number2 = null;
+            operator1 = null;
+            operator2 = null;
+            result=null;
+        }
     }
 }
 
-function inputDecimal() {
-
+function inputDecimal(dot) {
+    if (!displayValue.includes(dot)) {
+        displayValue += dot;
+    }
 }
 
-function inputPercent() {
-
+function inputPercent(num) {
+    displayValue = (num/100).toString();
 }
 
-function inputSign() {
-
+function inputSign(num) {
+    displayValue = (num * -1).toString();
 }
 
+//reset all variables for clear display -NOT innerhtml = '0';
 function clearDisplay() {
-
+    displayValue = '0';
+    number1 = null;
+    operator1 = null;
+    operator2 = null;
+    number2 = null;
+    result = null;
+    
 }
 
 
-function add(num1, num2) {
-    return num1 + num2;
-}
-  
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    if (num2 === 0) {
-        return "Error: Mommy Zero!";
-    }
-    return num1 / num2;
-}
-
-function operate(x, y, op) {
-    if (op === '+') {
-        return add(x,y)
-    }
-    else if (op === '-') {
-        return subtract(x,y)
-    }
-    else if (op === '*') {
-        return multiply(x,y)
-    }
-    else {
-        return divide(x,y)
-    }
-}
-
-function roundLongNumber(num, places) {
-    return parseFloat(Math.round(num + 'e' + places) + 'e-' + places);
-}
